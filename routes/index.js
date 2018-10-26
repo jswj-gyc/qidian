45fb802f7c7d2201548737c3471b6489b7ccbcd1
jswj-gyc <44466350+jswj-gyc@users.noreply.github.com>
2018-10-26 15:59:53 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,12 @@ const pool=require("../pool")
 
 router.get("/sel",(req,res)=>{
   var op=req.query.op
-  var num=req.query.num
-  var sql="SELECT * FROM book ORDER BY ? DESC LIMIT ?";
-  pool.query(sql,[op,num],(err,result)=>{
+  var num=parseInt(req.query.num)
+  var sql="SELECT * FROM book ORDER BY "+op+" desc limit 0,"+num;
+  pool.query(sql,[],(err,result)=>{
       if(err) console.log(err)
       console.log(sql,op,num)
-      //res.send(result)
-      res.writeHead(200,{
-          "Content-Type":"application/json;charset=utf-8",
-          "Access-Control-Allow-Origin":"*"
-      })
-      res.end()
+      res.send(result)
   })
 })
 
