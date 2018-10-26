e149d3469965362e9c3818f3e5ce5ac39bfcf7fd
jswj-gyc <44466350+jswj-gyc@users.noreply.github.com>
2018-10-26 16:02:26 +0800

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -21,18 +21,71 @@ $(function(){
   },3000);
   var op,num
   (async function(){
-    op="week"
-    num="10"
+    op="month"
+    num=10
     var res=await $.ajax({
       url:"http://localhost:3001/index/sel",
       type:"get",
       data:{op,num},
-      dataTyep:"json",
-      success:function(){
-        console.log(res)
-        
-      }
+      dataTyep:"json"
     })
-  })()
+    var $tui=$("div.tuijian>ul.tui")
+      var html="<li><h4>本周强推</h4></li>"
+      for(i=0;i<res.length;i++){
+        var {g,bname,aname}=res[i]
+          html+=`<li>
+          <a href="#">[${g}]</a>
+          <a href="#">${bname}</a>
+          <a href="#">${aname}</a>
+        </li>`
+      }
+      $tui.html(html)
+  })();
+  (async function(){
+    op="week"
+    num=10
+    var res=await $.ajax({
+      url:"http://localhost:3001/index/sel",
+      type:"get",
+      data:{op,num},
+      dataTyep:"json"
+    })
+    var $new=$("div.tuijian>ul.new")
+      var html="<li><h4>三江·网文新风</h4></li>"
+      for(i=0;i<res.length;i++){
+        var {g,bname,aname}=res[i]
+          html+=`<li>
+          <a href="#">[${g}]</a>
+          <a href="#">${bname}</a>
+          <a href="#">${aname}</a>
+        </li>`
+      }
+      $new.html(html)
+  })();
+  (async function(){
+    op="updata"
+    num=17
+    var res=await $.ajax({
+      url:"http://localhost:3001/index/sel",
+      type:"get",
+      data:{op,num},
+      dataTyep:"json"
+    })
+    var $updata=$("div.updata>ul>li>ul")
+      var html=""
+      for(i=0;i<res.length;i++){
+        var {g,bname,aname,updata}=res[i]
+          html+=`<li>
+          <a href="#">[${g}]</a>
+          <a href="#">《${bname}》</a>
+          <a href="#">第xxxx章 xxxxx</a> 
+          <a href="#">${aname}</a>
+          <span>${updata.slice(0,10)}</span>
+        </li>`
+      }
+      $updata.html(html)
+  })();
+  
+  
 })
 
